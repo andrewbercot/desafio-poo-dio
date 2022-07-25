12636bb6fcf1dcf594929268c535bac236632dcc
Andrew <andrew.bercot@gmail.com>
2022-07-25 11:52:15 -0300

    Add README.md

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args){
+
+        Curso curso1=new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Curso de Java");
+        curso1.setCargaHoraria(20);
+        System.out.println(curso1.toString());
+
+        Mentoria mentoria1=new Mentoria();
+        mentoria1.setData(LocalDate.now());
+        mentoria1.setDescricao("Mentoria");
+        mentoria1.setTitulo("Java POO");
+        System.out.println(mentoria1.toString());
+
+    }
 }
